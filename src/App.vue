81168249ee0f69bdb3cfd9ebab38effa378f311d
laiiihz <laiiihz@outlook.com>
2020-06-21 23:30:08 +0800

    [map]:add 4 layer of maps

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" style="height: 100%; width: 100%;">
+    {{ zoom }}
+    <l-map
+      style="height: 800px;width:980px"
+      :zoom="zoom"
+      @update:zoom="updateZoom"
+    >
+      <l-tile-layer
+        url="./img/{z}/map_{y}_{x}.png"
+        :tile-size="256"
+      ></l-tile-layer>
+    </l-map>
   </div>
 </template>
+<script>
+export default {
+  name: "app",
+  data: () => ({
+    url: "",
+    zoom: 0,
+  }),
+  methods: {
+    updateZoom(value) {
+      this.zoom = value;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import { LMap, LTileLayer } from "vue2-leaflet";
+import 'leaflet/dist/leaflet.css'
 
-Vue.config.productionTip = false
+Vue.component("l-map", LMap);
+Vue.component("l-tile-layer", LTileLayer);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
