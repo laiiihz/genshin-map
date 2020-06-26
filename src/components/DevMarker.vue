227a0f371379e20074ffae82955e2c34c35a2f88
laiiihz <laiiihz@outlook.com>
2020-06-26 19:50:04 +0800

    add portal anchors

--- src/components/DevMarker.vue
+++ src/components/DevMarker.vue
@@ -1,9 +1,29 @@
 <template>
-  <l-marker v-if="!disabled" :lat-lng="location" @update:lat-lng="handleUpdateLatLng" draggable>
-    <l-icon shadow-url="`#`" icon-url="./favicon.ico">
-      <i class="material-icons">location_on</i>
-    </l-icon>
-  </l-marker>
+  <div v-if="!disabled">
+    <div
+      style="
+        background-color: white;
+        position: fixed;
+        z-index: 4000;
+        top: 0;
+        left: 0;
+        height: 20px;
+      "
+    >
+      <label>
+        <input v-model="location" />
+      </label>
+    </div>
+    <l-marker
+      :lat-lng="location"
+      @update:lat-lng="handleUpdateLatLng"
+      draggable
+    >
+      <l-icon shadow-url="`#`" icon-url="./favicon.ico">
+        <i class="material-icons">location_on</i>
+      </l-icon>
+    </l-marker>
+  </div>
 </template>
 
 <script>
